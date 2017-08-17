e4a6e60fc22c688d8e3c269e29e0711433bc8a5c
Sophiα <12881223+Sophia2329@users.noreply.github.com>
2017-08-17 11:23:29 +0200

    f

--- WIP/js/tipso-auto.js
+++ WIP/js/tipso-auto.js
@@ -7,11 +7,11 @@ $(_=>{
 				background: 'white',
 				color: 'black',
 				width: '25vw',
-				maxWidth: '50vw'
+				maxWidth: '75vw'
 			}
 			
 			tps=(nm,p={})=>$("#tipso-"+nm).tipso(ext(tpsdef,ext({contentElementId: "tipsocontent-"+nm}, p)))
 			
-			$("tipso").toArray().map(x=>x.id.split("tipso-")[1]).forEach(x=>tps(x, {tooltipHover: $("#tipso-"+x).attr("tooltipHover")=="true"}))
+			$("tipso").toArray().map(x=>x.id.split("tipso-")[1]).forEach(x=>tps(x, {tooltipHover: $("#tipso-"+x).attr("tooltipHover")=="true", titleContent: (x=>x[0].toUpperCase()+x.slice(1))($("#tipso-"+x).text())}))
 			
 			})
